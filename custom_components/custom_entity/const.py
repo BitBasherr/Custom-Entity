f34c46ce2c097e84ed8b3bfd7ad24f22164c8751
BitBasherr <betalik@outlook.com>
2025-06-27 01:00:34 -0500

    fix missing constants

--- custom_components/custom_entity/const.py
+++ custom_components/custom_entity/const.py
@@ -5,3 +5,9 @@ CONF_FRIENDLY_NAME = "friendly_name"
 CONF_DEVICE_CLASS = "device_class"
 CONF_INHERIT_ATTRS = "inherit_attributes"
 CONF_PLATFORM = "platform"
+# Options-flow extras
+CONF_BATTERY_ENTITY    = "battery_entity"
+CONF_ATTRIBUTE_SENSORS = "attribute_sensors"    # {friendly → entity_id}
+CONF_COMBINE           = "combine"
+CONF_COMBINE_ENTITY    = "combine_entity"
+CONF_COMBINE_ATTR_NAME = "combine_attr_name"
